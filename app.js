f0049a7b55441bad81855ad8478d639bec7ca701
vikas singh <pokhriyalvikas940@gmail.com>
2023-12-23 10:09:45 +0530

    back on this

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const searchBtn = document.getElementById('search-btn');
 const search_result = document.getElementById('search-result');
 const show_more = document.getElementById('show-more');
 
-// window.addEventListener('load', ()=> fetchImages('good'));
+
 
 let keyword = '';
 let page = 1;
@@ -32,23 +32,31 @@ async function fetchImages() {
         // set the image in a tag
         imageLink.appendChild(image);
 
-        // send the image in div
-        search_result.appendChild(imageLink)
+        // append the image in div
+        search_result.appendChild(imageLink);
     })
 
     show_more.style.display = 'block';
 }
-
-show_more.addEventListener('click', () => {
-    page++
-    fetchImages();
-})
 
 
 
 searchBtn.addEventListener('click', () => {
     page = 1;
     fetchImages();
-})
+});
+
+
+
+show_more.addEventListener('click', () => {
+    page++
+    fetchImages();
+});
+
+
+
+
+
+
 
 
